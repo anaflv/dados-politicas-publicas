cd4ed252c7a7ca3a21091fd14cc07c6dfc8290d9
Ana Flávia de Araujo <aflvaraujo@gmail.com>
2018-08-15 23:12:14 -0300

    added school dm

---
+++ Scripts SQL/dm.NotasPorEscola_Avg.sql
@@ -0,0 +1,21 @@
+
+USE bd
+
+DROP TABLE dm.NotasPorEscola_Avg
+
+-- media de notas/escolas  
+SELECT 
+ COUNT(*)				[Quantidade]
+,SUM([Nota Redacao])    [Média Nota Redacao]
+,SUM([Nota Matematica]) [Média Nota Matematica]
+,SUM([Nota CH])			[Média Nota CH]
+,SUM([Nota CN])			[Média Nota CN]
+,SUM([Nota Enem 2013])	[Média Nota Enem]
+,SUM([Nota INSE AB])	[Média Nota INSE]
+,[Municipio]
+,[Tipo Escola]
+into dm.NotasPorEscola_Avg
+from  dw.Escolas
+group by [Municipio], [Tipo Escola]
+order by 2
+
